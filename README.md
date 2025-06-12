3b20b2cf1ac348002ff462f5109debe2a0563369
ARlynky <darrellrumagit@gmail.com>
2025-06-12 13:05:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# BOOKMARK
+
+This repo is for **personal use** and not intended for public consumption (yet!)
+I’m using it to learn how to share CLI tools and packages like a real developer.
+
+a script that allows you to add, remove, list, and of course goto a marked
+terminal directory
+
+---
+
+## 🛠 Installation
+
+> ⚠️ Only works on **Linux-based systems** (for now)
+
+To install this package, run the following command in your terminal:
+
+```sh
+curl -sSL https://raw.githubusercontent.com/ARlynky/bookmark/main/install.sh | bash
+```
+
+This will:
+
+* Clone the repo into `~/.local/share/bookmark/`
+* Add a launcher to `~/.local/bin/bookmark`
+* Let you run `bookmark` from anywhere (if `~/.local/bin` is in your `PATH`)
+
+## 🛠 Updating
+
+You can update it via running the install script again either from the
+repo (.local/share/install.sh) or from the web like before
+note, this will reset your bookmarks
---
+++ bookmark.py
@@ -0,0 +1,74 @@
+import argparse
+import json
+from pathlib import Path
+
+
+def mark(data_file, data, name):
+    current_dir = str(Path.cwd())
+    data[name] = current_dir
+    with open(data_file, "w") as f:
+        json.dump(data, f, indent=4)
+    print("Current directory bookmarked successfully")
+
+
+def listing(bookmarks):
+    for name, path in bookmarks.items():
+        print(name, ": ", path)
+
+
+def empty(data_file):
+    with open(data_file, "w") as f:
+        f.write("{}")
+
+
+def remove(data_file, data, name):
+    if name in data:
+        del data[name]
+        with open(data_file, "w") as f:
+            json.dump(data, f, indent=4)
+        print("Bookmark removed successfully")
+    else:
+        print("Bookmark not found")
+
+
+def goto(data, name):
+    if name in data:
+        path = data[name]
+        print(path)
+        # subprocess.run(["bash", "goto.sh", path])
+    else:
+        print("Bookmark not found")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Bookmark manager for terminal directory"
+    )
+    parser.add_argument("--mark", help="Bookmark current directory")
+    parser.add_argument("--list", help="List all bookmarks", action="store_true")
+    parser.add_argument("--empty", help="Empty bookmarks", action="store_true")
+    parser.add_argument("--remove", help="Remove a bookmark by name")
+    parser.add_argument("--goto", help="Goto a directory by name")
+
+    args = parser.parse_args()
+
+    data_file = "data.json"
+
+    with open(data_file, "r") as f:
+        bookmarks = json.load(f)
+
+    if args.mark:
+        mark(data_file, bookmarks, args.mark)
+
+    if args.list:
+        listing(bookmarks)
+
+    if args.empty:
+        empty(data_file)
+
+    if args.remove:
+        remove(data_file, bookmarks, args.remove)
+
+
+if __name__ == "__main__":
+    main()
